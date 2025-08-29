fc262f96d2a06bce2e2f1c788728a4478dcf9f56
Toufiqul2022 <toufiqul8865@gmail.com>
2025-08-29 21:28:28 +0600

    commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const history = []
+
 const loveCount = document.getElementById("love-count");
 const loveBtn = document.getElementsByClassName("love-btn");
 let count1 = 0;
@@ -34,3 +36,42 @@ for(const btn of callBtn){
     }
 })
 }
+
+document.getElementById("nation-call").addEventListener("click", function(){
+    const data = {
+        name : "National Emergency Number",
+        number : "999",
+        time: new Date().toLocaleTimeString()
+    }
+    history.push(data)
+})
+
+
+document.getElementById("history-click").addEventListener("click", function(){
+    const transactionContainer = document.getElementById("History-container")
+transactionContainer.innerText = ""
+
+    for(const data of transactionData){
+        const div = document.createElement("div")
+        div.innerHTML=`
+        <div class=" bg-white rounded-xl p-3 flex justify-between items-center mt-3">
+              <div class="flex items-center">
+                  <div class="p-3 rounded-full bg-[#f4f5f7]">
+                    <img src="./assets/wallet1.png" class="mx-auto" alt="" />
+                  </div>
+                  <div class="ml-3">
+                    <h1>${data.name}</h1>
+                    <p>${data.number}</p>
+                    <p>${data.time}</p>
+                  </div>
+              </div>
+      
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </div>
+        `
+
+        transactionContainer.appendChild(div)
+
+
+    }
+})
